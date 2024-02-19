51ecd577033daca994b2694ac33032949c0d2904
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-02-19 14:38:02 +0100

    [print] result in HTML page

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 //1
-const name = prompt('Inserisci il tuo nome');
-console.log('Nome: ', name);
+const name = prompt("Inserisci il tuo nome");
+console.log("Nome: ", name);
 
 //2
-const lastName = prompt('Inserisci il tuo cognome');
-console.log('Cognome: ', lastName);
+const lastName = prompt("Inserisci il tuo cognome");
+console.log("Cognome: ", lastName);
 
 //3
-const favouriteColor = prompt('Inserisci il tuo colore preferito');
-console.log('Colore preferito: ', favouriteColor);
+const favouriteColor = prompt("Inserisci il tuo colore preferito");
+console.log("Colore preferito: ", favouriteColor);
 
 //4
-const password = (name + lastName + favouriteColor + '24').toLowerCase();
+const password = (name + lastName + favouriteColor + "24").toLowerCase();
 
 //5
-console.log(password);
+console.log("Password: ", password);
+
+//6
+document.getElementById("output").innerHTML = `
+<p>
+La tua nuova password è: <strong>${password}</strong>
+</p>
+`;
