a9014c7fe75c94bdc1de7323dce205ef36c81fc2
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-02-19 14:56:23 +0100

    [add] style to output

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //1
-const name = prompt("Inserisci il tuo nome");
+const name = prompt("Inserisci il tuo nome:");
 console.log("Nome: ", name);
 
 //2
-const lastName = prompt("Inserisci il tuo cognome");
+const lastName = prompt("Inserisci il tuo cognome:");
 console.log("Cognome: ", lastName);
 
 //3
-const favouriteColor = prompt("Inserisci il tuo colore preferito");
+const favouriteColor = prompt("Inserisci il tuo colore preferito:");
 console.log("Colore preferito: ", favouriteColor);
 
 //4
